229a311e20f633d5a0964fa82f3f1a27b8324bdd
nlt1003 <nlt1003@sru.edu>
2022-02-03 10:22:41 -0500

    first commit

---
+++ Non-Lead Project/src/Elephant.java
@@ -0,0 +1,27 @@
+
+public class Elephant {
+
+	private String name;
+	private int trunk;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getTrunk() {
+		return trunk;
+	}
+
+	public void setTrunk(int trunk) {
+		this.trunk = trunk;
+	}
+
+	public static void main(String args[])
+	{
+		System.out.println("hello there");
+	}
+}
